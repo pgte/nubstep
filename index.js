268d876f242afebf087e606930240f318e998da5
Pedro Teixeira <pedro.teixeira@gmail.com>
2011-11-08 16:16:15 +0000

    exposing the envelope frequency

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var maestro = require('./lib/maestro')();
 var freq = 50;
+var envelopeFreq = 1;
+
 maestro.setFreq(freq);
+maestro.setEnvelopeFreq(envelopeFreq);
+
 maestro.start();
+
+setInterval(function() {
+  envelopeFreq +=0.1;
+  maestro.setEnvelopeFreq(envelopeFreq);
+}, 100);
 
 setTimeout(function() {
   maestro.stop();
